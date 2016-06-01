26b1f918b1c1d195fb76332fff7ad0c4485de9cc
Norbert Melzer <timmelzer@gmail.com>
2016-06-01 11:51:34 +0200

    start make run

--- src/runner.erl
+++ src/runner.erl
@@ -3,7 +3,7 @@
 -export([run/3]).
 
 -behaviour(gen_fsm).
--export([init/1]).
+-export([init/1, handle_info/3, terminate/3]).
 
 -export([warm_up/2]).
 
@@ -20,10 +20,30 @@ run(Problem, Parsed, Time) ->
 %% gen_fsm API
 
 init({Problem, Parsed, Time}) ->
-    gen_fsm:send_event_after(1000, {countdown, 5}),
+    gen_fsm:send_event_after(1000, {countdown, 10}),
+    Make = os:find_executable("make"),
+    io:format("~p~n", [Make]),
+    open_port({spawn_executable, Make}, [{args, [<<"run">>]},
+                                         exit_status]),
     {ok, warm_up, #state_data{problem = Problem,
                               parsed  = Parsed,
                               runtime = Time * 1000}}.
+
+handle_info({Port, {data, Line}}, warm_up, SD) ->
+    io:format("~s~n", [color:yellow(Line)]),
+    {next_state, warm_up, SD};
+handle_info({Port, {exit_status, 0}}, warm_up, SD) ->
+    io:format("~s~n", [color:red("Program exited unexpectedly but without any errors.")]),
+    {stop, {exit_early, 0}, SD};
+handle_info({Port, {exit_status, ES}}, warm_up, SD) ->
+    io:format("~s~n", [color:red("Program exited unexpected and with an error!")]),
+    io:format("~s: ~s~n~n", [color:red("Exit code"),
+                             color:redb(io_lib:format("~B", [ES]))]),
+    {stop, {exit_early, ES}, SD}.
+
+terminate(Reason, StateName, StateData) ->
+    ok.
+
 
 %% state handlers
 
