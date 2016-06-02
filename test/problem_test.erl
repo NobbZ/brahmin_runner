60d943abafb0e9e91ea05267cada08aa2ac0c23b
Norbert Melzer <timmelzer@gmail.com>
2016-06-03 01:08:38 +0200

    another test

--- test/problem_test.erl
+++ test/problem_test.erl
@@ -4,3 +4,10 @@
 
 no_whitespace_test() ->
     ?assertMatch(error, problem:check("[(3,3)]\n[( 3,3,1)]\n1\n")).
+
+perfect_answer_test() ->
+    Problem = "[(3,3)]\n[(3,3,10)]\n1\n",
+    Solution = "[[(0,0,1)]]\n",
+    {ok, ProblemParsed} = problem:check(Problem),
+    {ok, SolutionParsed} = solution:check(Solution),
+    ?assertMatch(1, solution:evaluate(SolutionParsed, ProblemParsed)).
