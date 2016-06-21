a5303aaadd5260d216242670bd41c4f490b6f6e7
Norbert Melzer <timmelzer@gmail.com>
2016-06-21 22:17:57 +0200

    exercise stuff

--- src/br_exercise.erl
+++ src/br_exercise.erl
@@ -1,16 +1,16 @@
 -module(br_exercise).
 
--export([new/3, get_bags/1]).
+-export([new/3, get_bags/1, get_rectangles/1, get_fill_cost/1]).
 
 -export_type([exercise/0]).
 
 -record(exercise, {bags  :: list(br_bag:bag()),
-                   rects :: list(br_rect:rect()),
+                   rects :: list(br_rectangle:rectangle()),
                    fill  :: non_neg_integer()}).
 
 -type exercise() :: #exercise{}.
 
--spec new(list(br_bag:bag()), list(br_rect:rect()), non_neg_integer())
+-spec new(list(br_bag:bag()), list(br_rectangle:rectangle()), non_neg_integer())
          -> exercise().
 new(Bags, Rects, Fill) ->
     #exercise{bags  = Bags,
@@ -19,6 +19,12 @@ new(Bags, Rects, Fill) ->
 
 -spec get_bags(exercise()) -> list(br_bag:bag()).
 get_bags(#exercise{bags = B}) -> B.
+
+-spec get_rectangles(exercise()) -> list(br_rectangle:rectangle()).
+get_rectangles(#exercise{rects = R}) -> R.
+
+-spec get_fill_cost(exercise()) -> non_neg_integer().
+get_fill_cost(#exercise{fill = F}) -> F.
 
 %% Local Variables:
 %% flycheck-erlang-include-path: ("../include")
